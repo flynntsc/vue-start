c7440a6b1a121397e18d799139a7f01a28432f0e
Flyn <flynntsc@qq.com>
2017-08-11 09:39:37 +0800

    clean up

--- webpack.config.js
+++ webpack.config.js
@@ -89,7 +89,7 @@ module.exports = (env = {}) => ({
     compress: true,
     proxy: {
       '/api/': {
-        target: 'http://127.0.0.1:8080',
+        target: 'http://127.0.0.1:8011',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
