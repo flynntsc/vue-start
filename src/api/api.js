7dc7efa19d71058d0b3c997de65a718a1d16778b
Flyn <flynntsc@qq.com>
2017-08-14 11:14:06 +0800

    更新api写法

--- src/api/api.js
+++ src/api/api.js
@@ -2,28 +2,20 @@ import axios from 'axios'
 
 // Defined
 const prefix = '/api'
-const fp = url => prefix + url
-const fn = (url, method) => params => {
-  switch (method) {
-    case 'post':
-      return axios.post(prefix + url, params)
-    case 'put':
-      return axios.put(prefix + url, params)
-    case 'del':
-      return axios.delete(prefix + url, params)
-    default:
-      return axios.get(prefix + url, params)
-  }
-}
+const link = url => prefix + url
+const get = (url, method) => params => axios.get(prefix + url, params)
+const post = (url, method) => params => axios.post(prefix + url, params)
+const put = (url, method) => params => axios.put(prefix + url, params)
+const del = (url, method) => params => axios.delete(prefix + url, params)
 
 // 获取用户信息 ->查
-export const getUser = fn('/user')
+export const getUser = get('/user')
 // 新增用户信息 ->增
-export const postUser = fn('/user', 'post')
+export const postUser = post('/user')
 // 更改用户信息 ->改
-export const putUser = fn('/user', 'put')
+export const putUser = put('/user')
 // 删除用户信息 ->删
-export const delUser = fn('/user', 'del')
+export const delUser = del('/user')
 
 // 上传地址
-export const pathFileUpload = fp('/uploadfile')
+export const pathFileUpload = link('/uploadfile')
