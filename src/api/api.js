b6b24e61ba8e60b6629491ecf8ddc8cc7092c5c6
Flyn <flynntsc@qq.com>
2017-08-15 16:32:00 +0800

    修正get&del方法参数格式

--- src/api/api.js
+++ src/api/api.js
@@ -3,10 +3,10 @@ import axios from 'axios'
 // Defined
 const prefix = '/api'
 const link = url => prefix + url
-const get = (url, method) => params => axios.get(prefix + url, params)
+const get = (url, method) => params => axios.get(prefix + url, { params })
 const post = (url, method) => params => axios.post(prefix + url, params)
 const put = (url, method) => params => axios.put(prefix + url, params)
-const del = (url, method) => params => axios.delete(prefix + url, params)
+const del = (url, method) => params => axios.delete(prefix + url, { params })
 
 // 获取用户信息 ->查
 export const getUser = get('/user')
